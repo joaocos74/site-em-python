79a2fc2fea6f54b8808c1534021296863062d912
joao <joaocrpm7@gmail.com>
2025-12-29 15:45:12 -0300

    base python

---
+++ main.py
@@ -0,0 +1,47 @@
+from flask import Flask, render_template, redirect, request
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'joao paulo'
+
+@app.route("/")
+def home():
+    return render_template("login.html")
+
+
+@app.route('/login', methods=['POST'])
+def login():
+    
+    nome = request.form.get('nome')
+    senha = request.form.get('senha')
+    print(nome)
+    print(senha)
+    
+    if nome == 'admin' and senha == '1234':
+        return render_template('usuarios.html')   
+    else:
+        
+        return redirect('/')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+    
