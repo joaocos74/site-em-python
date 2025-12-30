da6a62f5a1f7de1e8003a9712d616ce3624c8d4d
joao <joaocrpm7@gmail.com>
2025-12-30 12:47:57 -0300

    falta retirar duplicados

--- main.py
+++ main.py
@@ -5,13 +5,24 @@ import json
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'joao paulo'
 
+logado = False
+
 @app.route("/")
 def home():
+    global logado
+    logado = False
     return render_template("login.html")
 
+@app.route('/admin')
+def admin():
+    if logado == True:    
+        return render_template('admin.html')
+    if not logado == False:
+        return redirect('login.html')
 
 @app.route('/login', methods=['POST'])
 def login():
+    global logado
     
     nome = request.form.get('nome')
     senha = request.form.get('senha')
@@ -22,24 +33,35 @@ def login():
         
         for usuario in usuarios:
             cont += 1
+            if nome == 'adm' and senha == '123':
+                logado = True
+                return redirect('/admin')   
             if usuario['nome'] == nome and usuario['senha'] == senha:
                 return render_template('usuarios.html')
             if cont >= len(usuarios):
                 flash('Usuário ou senha incorretos!')
                 return redirect('/')    
+       
+@app.route('/cadastrarusuario', methods=['POST'])
+def cadastrarusuario():
+    user = []
+    nome = request.form.get('nome')
+    senha = request.form.get('senha')
+    user = [
+        {
+            "nome": nome,
+            "senha": senha
+        }
+    ] 
+    with open('usuarios.json') as usuariosTemp:
+        usuarios = json.load(usuariosTemp)
 
+    usuarioNovo = usuarios + user
+    
+    with open('usuarios.json', 'w') as gravarTemp:
+        json.dump(usuarioNovo, gravarTemp, indent=4)
 
-
-        
-        return redirect('/')
-
-
-
-
-
-
-
-
+    return redirect('/admin')
 
 
 
